1d9cbe5cbc82461cb4c948b504b65d32470fa062
Nickson kalulu <bsc-com-34-20@unima.ac.mw>
2025-03-25 05:16:33 +0000

    updated the create event DTO

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,8 +1,16 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateEventDto {
-    title: string;
-    date: Date;
-    location: string;
-    description: string;
-  }
-  
+  @ApiProperty({ description: 'The title of the event' })
+  title: string;
+
+  @ApiProperty({ description: 'A brief description of the event' })
+  description: string;
+
+  @ApiProperty({ description: 'Date of the event (ISO format)' })
+  date: Date;
+
+  @ApiProperty({ description: 'Location where the event will take place' })
+  location: string;
+}
